f12c6ddd95346ece14942867b5ce74021a294e53
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-09-06 14:34:45 +0200

    added type id on projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function type()
+    {
+        return  $this->belongsTo(Type::class);
+    }
 }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,19 +3,24 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
+
     public function run(): void
     {
+        $type_ids = Type::pluck("id")->toArray();
         //
         $new_project = new Project();
         $new_project->title = "Laravel Comics";
+        $new_project->type_id = Arr::random($type_ids);
         $new_project->url = "https://github.com/PasqualeDiMatteo/laravel-dc-comics";
         $new_project->description = "Copia del sito DC";
         $new_project->save();
